c3f673f2905a5c1f66ecb1ce87657aecc6abb48a
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-05-28 08:25:08 +0000

    1431: accepted

--- src/1431/1431.py
+++ src/1431/1431.py
@@ -13,3 +13,21 @@
 # - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
 # - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
 # - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+
+class Solution(object):
+
+    def f(self, i, candies, extraCandies):
+        return(all([candies[i] + extraCandies >= j for j in candies]))
+
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        return([self.f(i, candies, extraCandies) for i in range(len(candies))])
+        
+ 
+
+
+    
