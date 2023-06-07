37d21c2285e2f687f0541cb8f648bfb313cca6ea
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-06-07 15:39:24 +0100

    improvements to 345

--- src/345/345.py
+++ src/345/345.py
@@ -7,17 +7,18 @@ class Solution(object):
         vowel_idxs = []
         vowel_itms = []
         result = []
+        all_vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
+
         for i in range(len(s)):
             result.append(s[i])
-            if(s[i] in ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]):
+            if(s[i] in all_vowels):
                 vowel_idxs.append(i)
                 vowel_itms.append(s[i])
 
         vowel_itms.reverse()
 
         for i in range(len(vowel_idxs)):
-            print(i)
-            if(s[vowel_idxs[i]] in ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]):
+            if(s[vowel_idxs[i]] in all_vowels):
                 result[vowel_idxs[i]] = vowel_itms[i]
 
         return(''.join(result))
