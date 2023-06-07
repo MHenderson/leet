1a64c37a90c8312636b25965a06399d703745fd9
Matthew Henderson <matthew.james.henderson@gmail.com>
2023-06-07 15:37:03 +0100

    another crappy solution

--- src/345/345.py
+++ src/345/345.py
@@ -1,22 +1,23 @@
-s = "ab@a"
-expected = "aba@"
+class Solution(object):
+    def reverseVowels(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        vowel_idxs = []
+        vowel_itms = []
+        result = []
+        for i in range(len(s)):
+            result.append(s[i])
+            if(s[i] in ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]):
+                vowel_idxs.append(i)
+                vowel_itms.append(s[i])
 
-result = []
-vowels = []
+        vowel_itms.reverse()
 
-for i in range(len(s)):
-    if(s[i] in ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]):
-        vowels.append(s[i])
-        result.append("@")
-    else:
-        result.append(s[i])
+        for i in range(len(vowel_idxs)):
+            print(i)
+            if(s[vowel_idxs[i]] in ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]):
+                result[vowel_idxs[i]] = vowel_itms[i]
 
-vowels.reverse()
-
-for v in vowels:
-    for i in range(len(result)):
-        if(result[i]=="@"):
-            result[i]=v
-            break
-
-print(''.join(result))
+        return(''.join(result))
